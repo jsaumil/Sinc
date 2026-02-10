78efa15749c3e81030de46bd06cc32898b6b2337
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2026-02-10 16:07:07 +0530

    train fix

--- vanilla/train.py
+++ vanilla/train.py
@@ -35,7 +35,7 @@ class Config:
     vocab_size: int = 100278
     n_layer: int = 12
     n_head: int = 12
-    n_embd: int = 768
+    n_embd: int = 256
     sample_rate: int = 48000
     out_channels: int = 768
     kernel_size: int = 251

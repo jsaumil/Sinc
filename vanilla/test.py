7b695fc690b9536cf95f6dcb1b684f71214c61e6
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2026-02-10 15:59:33 +0530

    train fix

---
+++ vanilla/test.py
@@ -0,0 +1,27 @@
+import torch
+from model import Sona
+from dataclasses import dataclass
+
+device="cpu"
+
+@dataclass
+class Config:
+    block_size: int = 1024
+    vocab_size: int = 100278
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+    sample_rate: int = 48000
+    out_channels: int = 768
+    kernel_size: int = 251
+    stride: int = 1
+    padding: int = 0
+
+
+model = Sona(Config.sample_rate,Config(vocab_size=100278))
+from torchinfo import summary
+audio = torch.randn(1, 1, 160797).to(device=device)                    # dummy audio batch
+text  = torch.randint(0, 100278, (1, 39)).to(device=device)
+print(text.shape)
+
+summary(model, input_data=[audio, text])

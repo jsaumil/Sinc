354031c370dbbc7622afa9dc889501951467b988
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2026-02-12 14:48:38 +0530

    reduce the training size

--- vanilla/train.py
+++ vanilla/train.py
@@ -35,9 +35,9 @@ class Config:
     vocab_size: int = 100278
     n_layer: int = 12
     n_head: int = 12
-    n_embd: int = 384
+    n_embd: int = 192
     sample_rate: int = 48000
-    out_channels: int = 384
+    out_channels: int = 192
     kernel_size: int = 251
     stride: int = 1
     padding: int = 0
